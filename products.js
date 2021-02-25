f7006f867524850efb8e337529c187b04ed60f62
deliciads <delicia1999@yahoo.com>
2021-02-25 13:20:09 +0400

    'first'

---
+++ products.js
@@ -0,0 +1,39 @@
+
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 pound bad of <em>irresistible</em>," + "organic goodness for your cat.",
+        "price": 20.00,
+        "image": "cat-food.jpg",
+        "availableInventory": 10,
+        "rating": 3 
+    },
+    {
+        "id": 1002,
+        "title": "Yarn",
+        "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+        "price": 2.99,
+        "image": "yarn.jpg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1003,
+        "title": "Kitty Litter",
+        "description": "Premium kitty litter for your cat.",
+        "price": 15.00,
+        "image": "kitten-litter.png",
+        "availableInventory": 10,
+        "rating": 4
+    },
+    {
+        "id": 1004,
+        "title": "Cat house",
+        "description": "Cat house for your cat to rest.",
+        "price": 35.00,
+        "image": "cat-house.jpg",
+        "availableInventory": 10,
+        "rating": 5
+    }
+]
