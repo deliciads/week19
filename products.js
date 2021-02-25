1bbe6065f31c714649ca98a9e56628fdeda724cd
deliciads <delicia1999@yahoo.com>
2021-02-25 13:37:07 +0400

    image path

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ let products = [
         "title": "Cat Food, 25lb bag",
         "description": "A 25 pound bad of <em>irresistible</em>," + "organic goodness for your cat.",
         "price": 20.00,
-        "image": "images/cat-food.jpg",
+        "image": "Images/cat-food.jpg",
         "availableInventory": 10,
         "rating": 3 
     },
@@ -14,7 +14,7 @@ let products = [
         "title": "Yarn",
         "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
         "price": 2.99,
-        "image": "images/yarn.jpg",
+        "image": "Images/yarn.jpg",
         "availableInventory": 10,
         "rating": 2
     },
@@ -23,7 +23,7 @@ let products = [
         "title": "Kitty Litter",
         "description": "Premium kitty litter for your cat.",
         "price": 15.00,
-        "image": "images/kitten-litter.png",
+        "image": "Images/kitten-litter.png",
         "availableInventory": 10,
         "rating": 4
     },
@@ -32,7 +32,7 @@ let products = [
         "title": "Cat house",
         "description": "Cat house for your cat to rest.",
         "price": 35.00,
-        "image": "images/cat-house.jpg",
+        "image": "Images/cat-house.jpg",
         "availableInventory": 10,
         "rating": 5
     }
