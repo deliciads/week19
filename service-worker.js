7790e6ce09d13acd1a24fee80bd028388dd75ada
deliciads <delicia1999@yahoo.com>
2021-02-25 14:37:42 +0400

    update

--- service-worker.js
+++ service-worker.js
@@ -19,3 +19,25 @@ self.addEventListener('install', (e) => {
         })
     );
 });
+
+/*self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            console.log('[Sevice Worker] Fetching resource: ' + e.request.url);
+            return r
+        })
+    );
+});*/
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (response) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
