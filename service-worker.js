0610616dfa6f5e3766f6e6019e5df23578eaa451
deliciads <delicia1999@yahoo.com>
2021-02-25 13:50:55 +0400

    service worker

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+var chacheName = 'petstore-v1';
+var cacheFiles = [
+    'petstore.html',
+    'product.js',
+    'petstore.webmanifest',
+    'images/cat-food.jpg',
+    'images/yarn.jpg',
+    'images/kitten-litter.png',
+    'images/cat-house.jpg'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
